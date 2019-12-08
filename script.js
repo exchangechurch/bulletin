64d82e5d6659de7edb3206f62e5ec7eaaa8c1473
Gilbert Kim <gbertkim@gmail.com>
2019-12-08 12:10:08 -0800

    first commit wireframe

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict'
+
+function clicky(){
+    $('#arrow').on('click', function(event){
+        event.preventDefault();
+        console.log('hello');
+        $('#infoContainer').slideUp();
+    });
+}
+
+let pageLoad = function(){
+    $('.barLink').on('click', function(event){
+        var linkAttr = $(this).attr("href");
+        console.log(linkAttr + " was clicked")
+        $(linkAttr).siblings().hide();
+        $(linkAttr).show();
+    })
+    $('#logo').on('click', function (event){
+        var linkAttr = $(this).attr("href");
+        console.log(linkAttr + " was clicked")
+        $('#home').show();
+        $('#home').siblings().hide();
+    })
+  }
+
+$(document).ready(function(){
+    console.log("Page is loaded");
+    $('#home').siblings().hide();
+    clicky();
+    pageLoad();
+  });
