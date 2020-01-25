126adf799a96b81e6b953d510c8cf9b8a39f4549
Gilbert Kim <gbertkim@gmail.com>
2020-01-25 02:41:04 -0800

    Changed with design requests

--- script.js
+++ script.js
@@ -1,31 +1,109 @@
 'use strict'
 
-function clicky(){
-    $('#arrow').on('click', function(event){
-        event.preventDefault();
-        console.log('hello');
-        $('#infoContainer').slideUp();
-    });
+let announcementsStore = [{
+    title: 'Read the Word 2020',
+    date: 'January 2020',
+    url: 'images/1.jpg',
+    alt: 'Image of 5 day plan',
+    description: 'As we begin 2020, let us commit to reading the Word of God together in community. We will be providing a reading plan that will take us through the entire bible this year. It is called the 5 day plan and is also available on the Dwell App. We highly encourage you to download and use this app, particularly if you want to grow in God’s Word but have found it difficult to stick to a reading plan. If you’d like a printed schedule for January, one will be available at our Connect Table.'
+    },{
+    title: 'Join a Life Group',
+    date: 'January 2020',
+    url: 'images/2.jpg',
+    alt: 'image of faded bibles',
+    description: 'A Life Group is a small group of men or women that shares in life as they grow into their Christ-defined identities together. Life groups gather weekly to remember and apply the gospel, grow in transparency through sharing and confession, and learn to actively follow the Spirit. If you are not a covenant member of the Exchange Church and are interested in joining a life group for next quarter (10 weeks starting the second week of Jan), please sign up at the welcome table.'
+},];
+
+let orderOfWorship = [{
+    title: 'CALL TO WORSHIP',
+    description: 'We like to begin worship by directing our hearts toward the glory and grace of God. We do this by reading a brief passage of scripture and meditating on it together.'
+    },{
+    title: 'WORSHIP IN SONG',
+    description: 'Although everything in our lives is meant to be worship to God, we believe that a worshipful life is cultivated through the gathering of believers in a corporate setting. Singing in these gatherings helps us to be educated theologically, engage God emotionally, and express our unity as we celebrate the gospel together.'
+    },{
+    title: 'CONFESSION OF FAITH',
+    titleAlt: '(APOSTLE’S CREED)',
+    description: `This creed is called the Apostles' Creed not because it was produced by the apostles themselves but because it contains a summary of their teachings. It sets forth our beliefs in a beautiful simplicity, brevity and order. For believers, reciting the Apostles Creed each Sunday allows us to remember and affirm our faith in God the Father, the person and work of Jesus Christ, the Holy Spirit, and the Universal (Christian) Church.`
+    }
+    ,{
+    title: 'THE PREACHING OF GOD’S WORD',
+    description: 'The Bible is God’s inspired, perfect, powerful Word. The center of our corporate worship gathering is the preaching of God’s Word. Christ is the center of our worship and it is His teaching and commands that we are here to corporately hear and submit to.'
+    },{
+    title: 'GIVING OFFERING',
+    description: 'We believe that giving is a personal and private act of worship. If you are new or just visiting today, please do not feel obligated to give. For those of us who have prepared an offering for today, let’s give freely and cheerfully out of a response to God’s generous grace.'
+    }
+    ,{
+    title: 'BENEDICTION',
+    description: 'A benediction is a declaration of blessing from God upon His beloved people. They offer words of assurance designed to bring joy, peace, comfort, and security to those who place their trust in God. We close each worship service this way that we might leave blessed and be empowered to bless others.'
+}]
+
+
+function fillAnnouncements(){
+    for(let i=0; i < announcementsStore.length; i++){
+        $('#announceBody').append(`
+            <img class="announceImg" src=${announcementsStore[i].url} alt=${announcementsStore[i].alt}>
+            <div class="announceDetCont">
+                <h2 class="announceTitle">${announcementsStore[i].title}</h2>
+                <h3 class="announceDate">${announcementsStore[i].date}</h3>
+                <p class="announceDesc">${announcementsStore[i].description}</p>
+            </div>
+        `)
+    }
 }
 
-let pageLoad = function(){
-    $('.barLink').on('click', function(event){
-        var linkAttr = $(this).attr("href");
-        console.log(linkAttr + " was clicked")
-        $(linkAttr).siblings().hide();
-        $(linkAttr).show();
-    })
-    $('#logo').on('click', function (event){
-        var linkAttr = $(this).attr("href");
-        console.log(linkAttr + " was clicked")
-        $('#home').show();
-        $('#home').siblings().hide();
-    })
-  }
+function fillOrder(){
+    for(let i=0; i < orderOfWorship.length; i++){
+        console.log(orderOfWorship[i].titleAlt)
+        if (orderOfWorship[i].titleAlt === undefined){
+            $('#orderBody').append(`
+                <div class="orderWorshipContainer">
+                    <h2 class="orderTitle">${orderOfWorship[i].title}</h2>
+                    <p class="orderDesc">${orderOfWorship[i].description}</p>
+                </div>
+            `)
+        } else {
+            $('#orderBody').append(`
+            <div class="orderWorshipContainer">
+                <h2 class="orderTitle">${orderOfWorship[i].title}</h2>
+                <h2 class="orderTitleAlt">${orderOfWorship[i].titleAlt}</h2>
+                <p class="orderDesc">${orderOfWorship[i].description}</p>
+            </div>
+        `)
+        }
+    }
+}
+
+function fillContact(){
+    $('#contactBody').append(`
+        <div class="contactContainer">
+            <p class='contactDesc'>The Exchange Church<br>2341 W Lincoln Ave Anaheim, CA 92801</p>
+            <a id='webLink' href='http://www.the-exchange-church.com'>the-exchange-church.com</a>
+            <p class='contactDesc'>Joe Suh, Lead Pastor <br><a href='mailto:pastorjoe@the-exchange-church.com'>pastorjoe@the-exchange-church.com</a></p>
+            <p class='contactDesc'>Randy Cho, Associate Pastor <br><a href='mailto:randy@the-exchange-church.com'>randy@the-exchange-church.com</a></p>
+        </div>
+    `)
+}
+
+function expand(){
+    $('.barLink').click(function() {
+        if ($('.accordionBody').is(':visible')) {
+            $(".accordionBody").slideUp(300);
+            $(".expand").delay(750).text('+');
+        }
+        if ($(this).parent().next(".accordionBody").is(':visible')) {
+            $(this).parent().next(".accordionBody").slideUp(300);
+            $(this).children(".expand").delay(500).text('+');
+        } else {
+            $(this).parent().next(".accordionBody").slideDown(300);
+            $(this).children(".expand").delay(500).text('-');
+        }
+      });
+}
 
 $(document).ready(function(){
+    fillAnnouncements();
+    fillOrder();
+    fillContact();
+    expand();
     console.log("Page is loaded");
-    $('#home').siblings().hide();
-    clicky();
-    pageLoad(); 
   });
