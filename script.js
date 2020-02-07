20065505a1e7a1ff41d52b518cf8d3b13824683c
Gilbert Kim <gbertkim@gmail.com>
2020-02-07 14:06:31 -0800

    Added an object for sermon series

--- script.js
+++ script.js
@@ -1,42 +1,62 @@
 'use strict'
+let sermonStore = [{
+    date: `February 9th, 2020`,
+    series: `Created to Be`,
+    title: `In Christ`,
+    speaker: `Pastor Joe Suh`
+}];
 
 let announcementsStore = [{
-    title: 'Read the Word 2020',
-    date: 'January 2020',
-    url: 'Images/Read.jpg',
-    alt: 'Image of 5 day plan',
-    description: 'As we begin 2020, let us commit to reading the Word of God together in community. We will be providing a reading plan that will take us through the entire bible this year. It is called the 5 day plan and is also available on the Dwell App. We highly encourage you to download and use this app, particularly if you want to grow in God’s Word but have found it difficult to stick to a reading plan. If you’d like a printed schedule for January, one will be available at our Connect Table.'
+    title: `Read the Word 2020`,
+    date: `January 2020`,
+    url: `Images/Read.jpg`,
+    alt: `Image of 5 day plan`,
+    description: `As we begin 2020, let us commit to reading the Word of God together in community. We will be providing a reading plan that will take us through the entire bible this year. It is called the 5 day plan and is also available on the Dwell App. We highly encourage you to download and use this app, particularly if you want to grow in God’s Word but have found it difficult to stick to a reading plan. If you’d like a printed schedule for January, one will be available at our Connect Table.`
     },{
-    title: 'Join a Life Group',
-    date: 'January 2020',
-    url: 'Images/Join.jpg',
-    alt: 'image of faded bibles',
-    description: 'A Life Group is a small group of men or women that shares in life as they grow into their Christ-defined identities together. Life groups gather weekly to remember and apply the gospel, grow in transparency through sharing and confession, and learn to actively follow the Spirit. If you are not a covenant member of the Exchange Church and are interested in joining a life group for next quarter (10 weeks starting the second week of Jan), please sign up at the welcome table.'
+    title: `Join a Life Group`,
+    date: `January 2020`,
+    url: `Images/Join.jpg`,
+    alt: `image of faded bibles`,
+    description: `A Life Group is a small group of men or women that shares in life as they grow into their Christ-defined identities together. Life groups gather weekly to remember and apply the gospel, grow in transparency through sharing and confession, and learn to actively follow the Spirit. If you are not a covenant member of the Exchange Church and are interested in joining a life group for next quarter (10 weeks starting the second week of Jan), please sign up at the welcome table.`
 },];
 
 let orderOfWorship = [{
-    title: 'CALL TO WORSHIP',
-    description: 'We like to begin worship by directing our hearts toward the glory and grace of God. We do this by reading a brief passage of scripture and meditating on it together.'
+    title: `CALL TO WORSHIP`,
+    description: `We like to begin worship by directing our hearts toward the glory and grace of God. We do this by reading a brief passage of scripture and meditating on it together.`
     },{
-    title: 'WORSHIP IN SONG',
-    description: 'Although everything in our lives is meant to be worship to God, we believe that a worshipful life is cultivated through the gathering of believers in a corporate setting. Singing in these gatherings helps us to be educated theologically, engage God emotionally, and express our unity as we celebrate the gospel together.'
+    title: `WORSHIP IN SONG`,
+    description: `Although everything in our lives is meant to be worship to God, we believe that a worshipful life is cultivated through the gathering of believers in a corporate setting. Singing in these gatherings helps us to be educated theologically, engage God emotionally, and express our unity as we celebrate the gospel together.`
     },{
-    title: 'CONFESSION OF FAITH',
-    titleAlt: '(APOSTLE’S CREED)',
+    title: `CONFESSION OF FAITH`,
+    titleAlt: `(APOSTLE’S CREED)`,
     description: `This creed is called the Apostles' Creed not because it was produced by the apostles themselves but because it contains a summary of their teachings. It sets forth our beliefs in a beautiful simplicity, brevity and order. For believers, reciting the Apostles Creed each Sunday allows us to remember and affirm our faith in God the Father, the person and work of Jesus Christ, the Holy Spirit, and the Universal (Christian) Church.`
     }
     ,{
-    title: 'THE PREACHING OF GOD’S WORD',
-    description: 'The Bible is God’s inspired, perfect, powerful Word. The center of our corporate worship gathering is the preaching of God’s Word. Christ is the center of our worship and it is His teaching and commands that we are here to corporately hear and submit to.'
+    title: `THE PREACHING OF GOD’S WORD`,
+    description: `The Bible is God’s inspired, perfect, powerful Word. The center of our corporate worship gathering is the preaching of God’s Word. Christ is the center of our worship and it is His teaching and commands that we are here to corporately hear and submit to.`
     },{
-    title: 'GIVING OFFERING',
-    description: 'We believe that giving is a personal and private act of worship. If you are new or just visiting today, please do not feel obligated to give. For those of us who have prepared an offering for today, let’s give freely and cheerfully out of a response to God’s generous grace.'
+    title: `GIVING OFFERING`,
+    description: `We believe that giving is a personal and private act of worship. If you are new or just visiting today, please do not feel obligated to give. For those of us who have prepared an offering for today, let’s give freely and cheerfully out of a response to God’s generous grace.`
     }
     ,{
-    title: 'BENEDICTION',
-    description: 'A benediction is a declaration of blessing from God upon His beloved people. They offer words of assurance designed to bring joy, peace, comfort, and security to those who place their trust in God. We close each worship service this way that we might leave blessed and be empowered to bless others.'
+    title: `BENEDICTION`,
+    description: `A benediction is a declaration of blessing from God upon His beloved people. They offer words of assurance designed to bring joy, peace, comfort, and security to those who place their trust in God. We close each worship service this way that we might leave blessed and be empowered to bless others.`
 }]
 
+function fillSermon() {
+    $('#date').text(`
+        ${sermonStore[0].date}
+    `);
+    $('#sermonSeries').text(`
+        Sermon Series: ${sermonStore[0].series}
+    `);
+    $('#sermonTitle').text(`
+        Title: ${sermonStore[0].title}
+    `);
+    $('#speakerName').text(`
+        Speaker: ${sermonStore[0].speaker}
+    `);
+}
 
 function fillAnnouncements(){
     for(let i=0; i < announcementsStore.length; i++){
@@ -100,6 +120,7 @@ function expand(){
 }
 
 $(document).ready(function(){
+    fillSermon();
     fillAnnouncements();
     fillOrder();
     fillContact();
