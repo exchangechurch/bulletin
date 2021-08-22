c8b118cc8f30e043423fc1a2a3dc9d964ea3dc00
jfkung <jasonkung@me.com>
2021-08-22 10:26:01 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -42,10 +42,10 @@ let announcementsStore = [
     linkText: `Join us Online`
     },{
     title: `Movie Night`,
-    date: `September 11, 2021 4:00PM`,
+    date: `September 11, 2021 6:00PM`,
     url: `"Images/Movie Night Sept 2021.jpg"`, //spaces in the file name requires double quotes "" around it.
     alt: `image of theatre`,
-    description: `Come join us on September 11 at 4PM in the social hall for Movie Night! More details to come.`
+    description: `Come join us on September 11 at 6PM in the social hall for Movie Night! More details to come.`
     //link: `https://www.the-exchange-church.com/live`,
     //linkText: `Join us Online`
     }/*,{
